d07a1d47aba004cf37c074722855042f99c5571c
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2019-11-14 11:30:14 +0100

    Add files via upload

---
+++ zadacaOcjenee/zadacaOcjenee/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacaOcjenee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena;
+            double prosjek;
+            float brojOcjena = 0; //brojac
+            int zbrojOcjena = 0;
+            do
+            {
+                Console.WriteLine("Upisite ocjenu: ");
+                ocjena = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if (ocjena == 1)
+                {
+                    prosjek = 1;
+                    break;
+                }
+
+                else if (ocjena > 5 || ocjena < 0)
+                {
+                    Console.WriteLine("Pogresan unos. Unesite ponovno");
+
+                }
+                else if(ocjena!=0)
+                {
+                    brojOcjena+=1;
+                    zbrojOcjena += ocjena;
+                    
+                }
+                prosjek = zbrojOcjena / brojOcjena;
+
+            }
+            while (ocjena != 0);
+            Console.WriteLine("Prosjek ocjena je"+prosjek);
+            Console.ReadKey();
+        }
+    }
+}
